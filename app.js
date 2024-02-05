348271c6d84bdfd6ae968ad63e1c6bab6d3c56b6
mmcclanahan <mmmcclan365@gmail.com>
2024-02-05 11:55:32 -0800

    finished solo sprint

--- app.js
+++ app.js
@@ -1,1 +1,34 @@
 // TODO
+//create a GroceryList component
+//contains an UL of 2 grocery items and render it
+const App = () => {
+  return (
+  <div>
+    <h2>My Grocery List</h2>
+      <GroceryList items={['cucumber','kale','banana']}/>
+  </div>
+)};
+
+const GroceryList = (props) => (
+  <ul>
+    {props.items.map((item) => (
+      <GroceryListItem item={item}/>
+    ))}
+  </ul>
+);
+const { useState } = React;
+const GroceryListItem = (props) => {
+  const [isSelected, setIsSelected] = useState(false);
+  let style = {
+    fontWeight: isSelected ? 'bold' : 'normal',
+  };
+
+  const mouseOn = () => {setIsSelected(true)}
+  const mouseOff = () => {setIsSelected(false)}
+  return (<li style={style} onMouseEnter={mouseOn} onMouseLeave={mouseOff}>{props.item}</li>);
+};
+
+ReactDOM.render(
+  <App />,
+  document.getElementById('app')
+);
